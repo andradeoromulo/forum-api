01106348cda9b8f2dc504c9bd54cca93d2f06ce7
Romulo Andrade <andrade.o.romulo@gmail.com>
2021-04-28 00:17:34 -0300

    Add conexão com banco de dados H2 + endpoints para CRUD de Tópico

---
+++ src/main/java/br/com/alura/forumapi/controller/TopicosController.java
@@ -0,0 +1,99 @@
+package br.com.alura.forumapi.controller;
+
+import br.com.alura.forumapi.controller.dto.TopicoDTO;
+import br.com.alura.forumapi.controller.dto.TopicoDetalhadoDTO;
+import br.com.alura.forumapi.controller.form.AtualizaTopicoForm;
+import br.com.alura.forumapi.controller.form.TopicoForm;
+import br.com.alura.forumapi.model.Topico;
+import br.com.alura.forumapi.repository.CursoRepository;
+import br.com.alura.forumapi.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
+    public List<TopicoDTO> lista(String nomeCurso) {
+
+        List<Topico> topicos = null;
+
+        if(nomeCurso == null)
+            topicos = topicoRepository.findAll();
+        else
+            topicos = topicoRepository.findByCursoNome(nomeCurso);
+
+        return TopicoDTO.converter(topicos);
+
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
+
+        Topico topico = topicoForm.converter(cursoRepository);
+        topicoRepository.save(topico);
+
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+
+    }
+
+    @GetMapping("/{topicoId}")
+    public ResponseEntity<TopicoDetalhadoDTO> detalhar(@PathVariable Long topicoId) {
+
+        Optional<Topico> topico = topicoRepository.findById(topicoId);
+
+        if(topico.isPresent())
+            return ResponseEntity.ok(new TopicoDetalhadoDTO(topico.get()));
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @PutMapping("/{topicoId}")
+    @Transactional
+    public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long topicoId, @RequestBody AtualizaTopicoForm topicoForm) {
+
+        Optional<Topico> optional = topicoRepository.findById(topicoId);
+
+        if(optional.isPresent()) {
+            Topico topico = topicoForm.atualizar(optional.get());
+            return ResponseEntity.ok(new TopicoDTO(optional.get()));
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping("/{topicoId}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long topicoId) {
+
+        Optional<Topico> optional = topicoRepository.findById(topicoId);
+
+        if(optional.isPresent()) {
+            topicoRepository.deleteById(topicoId);
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+}
---
+++ src/main/java/br/com/alura/forumapi/repository/TopicoRepository.java
@@ -0,0 +1,12 @@
+package br.com.alura.forumapi.repository;
+
+import br.com.alura.forumapi.model.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    List<Topico> findByCursoNome(String nomeCurso);
+
+}
